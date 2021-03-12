5971bf1417eca57872bd4d88f9583c9925bfb203
Charles Mulinga <mulingacharles@gmail.com>
2021-03-12 12:19:04 +0300

    made some changes and incorporated jQuuery

---
+++ README.md
@@ -0,0 +1,12 @@
+# Akan Name Generator
+
+This is a web application that is inspired by the Ghanian culture. In Ghana the first name given to a newborn baby is solely dependent on the day of the week he/she is born and their gender.This name is known as the Akan name. The website i have created enables one to get an Akan name after input of their birthdate and gender. Thanks to the capabalities of javascript ,the webpage is able to use the input provided to compute a name.
+
+#### By Melissa Wangui 
+## Technologies used
+This webpage was created entirely by HTML5,CSS3 and JavaScript
+## Link to live webpage
+<https://melissa-koi.github.io/week2IP/>
+## License
+<https://github.com/melissa-koi/week2IP/blob/main/LICENSE><br>
+The license used is MIT
---
+++ script.js
@@ -0,0 +1,47 @@
+let resultPar = document.getElementById('output')
+
+function getInput(event) {
+    event.preventDefault();
+    let yourName = document.getElementById('name').value
+    let birthdate = document.getElementById('bday').value
+    let myBdate = new Date(birthdate).toDateString()
+    let day = myBdate.slice(0, 3);
+
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+
+    if (gender === "male" && day === "Sun") {
+        Name = "Kwasi"
+    } else if (gender === "male" && day === "Mon") {
+        Name = "Kwadwo"
+    } else if (gender === "male" && day === "Tue") {
+        Name = "Kwabena"
+    } else if (gender === "male" && day == "Wed") {
+        Name = "Kwaku"
+    } else if (gender === "male" && day === "Thu") {
+        Name = "Yaw"
+    } else if (gender === "male" && day === "Fri") {
+        Name = "Kofi"
+    } else if (gender === "male" && day === "Sat") {
+        Name = "Kwame"
+    } else if (gender === "female" && day === "Sun") {
+        Name = "Akosua"
+    } else if (gender === "female" && day === "Mon") {
+        Name = "Adwoa"
+    } else if (gender === "female" && day === "Tue") {
+        Name = "Abenaa"
+    } else if (gender === "female" && day === "Wed") {
+        Name = "Akua"
+    } else if (gender === "female" && day === "Thu") {
+        Name = "Yaa"
+    } else if (gender === "female" && day === "Fri") {
+        Name = "Afua"
+    } else if (gender === "female" && day === "Sat") {
+        Name = "Ama"
+    } else {
+        Name = "Invalid answer";
+    }
+
+    let message = `${yourName}, Your Akan name is ${Name}`
+    resultPar.innerHTML = message;
+
+}
